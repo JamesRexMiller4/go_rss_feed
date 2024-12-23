2130ccea254b1c0c454a5c9424f67c23cc3c5b24
James Miller <james.rex.miller.4@gmail.com>
2024-12-23 15:14:15 -0700

    Initial commit - add internal and config

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JamesRexMiller4/go_rss_feed
+
+go 1.22.1
---
+++ internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	db_url            string
+	current_user_name string
+}
+
+func Read() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Errorf("home directory could not be identified", err)
+	}
+
+	fmt.Printf(homeDir)
+}
+
+func (c Config) SetUser() {
+	// sets the current_user_name to struct
+	// then writes the Config struct back to JSON ~/.gatorconfig.json
+}
+
+func getConfigPath() (string, error) {
+
+}
+
+func write(cfg Config) error {
+
+}
